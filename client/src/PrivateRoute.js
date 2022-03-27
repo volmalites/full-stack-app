23324781f59057293536f2b61ba3d533e932bf39
Bjorn Lottering (Volmalites) <bjorn@octonet.co.za>
2022-03-27 11:48:22 +0200

    Added comment describing PrivateRoute functionality

--- client/src/PrivateRoute.js
+++ client/src/PrivateRoute.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { Consumer } from './Context';
 
+// Globally protected routes to prevent access for users not logged in
 const PrivateRoute =  ({ component: Component, ...rest }) => {
   return (
     <Consumer>
